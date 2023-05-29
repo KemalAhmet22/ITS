9c4935093d91a42245c9b57031eebe66f391e7ec
TahaErken <erken.taha@gmail.com>
2023-05-29 20:13:56 +0000

    Checksum tamamlandı

--- wecdis.py
+++ wecdis.py
@@ -20,9 +20,11 @@ class Read:
             outputstring = message.hex()
             if sens_mesaj[0].__contains__(b'VTG'):
                 obj_2.updateVTG(sens_mesaj,sens_mesaj2)
-                obj_3.checksum(outputstring)
-            #print(sens_mesaj)
-            #print (sens_mesaj2)
+                check = obj_3.checksum(outputstring)
+                cexk2 = str(sens_mesaj2[1])
+                if cexk2.__contains__(check):  #contains dışı
+                    print("Checksum Doğru")
+
 
 class VTG:
     def updateVTG(self, msg,msg2):
@@ -34,33 +36,34 @@ class VTG:
 
         print("Speed over Ground  kilometer=", msg[7], msg[8])
 
-        print("Checksum = ",msg2[0])
+        print("Checksum = ",msg2[1])
 
 
+
+def xor_strings(str1, str2):
+    result = ""
+    for i in range(len(str2)):
+        int1 = int(str1[i], 16)
+        int2 = int(str2[i], 16)
+
+        xor_results = int1 ^ int2
+
+        result += hex(xor_results)[2:]
+
+    return result
 
 class ChecksumCalc:
+
     def checksum(self,outputstring):
-
-
         hexlist = wrap(outputstring,2)
         for i in range(3):
             del hexlist[len(hexlist)-1]
-
         del hexlist[0]
-
-        checksum = 0
+        checksum2 = "00"
         for i in range(len(hexlist)):
-            hexlist[i] = "0x"+ hexlist[i]
-
-        for i in range(len(hexlist)):
-            converted = int(hexlist[i], 16)
-            checksum = hex(checksum ^ converted)
-
-        print(checksum)
-
-        print(hexlist)
-
-
+            checksum2 = xor_strings(checksum2,hexlist[i])
+        print(checksum2)
+        return checksum2
 
 obj_1 = Read()
 obj_2 = VTG()
